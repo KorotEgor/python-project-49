ad3af7f384fb108a4457650397bcfcab82b09ca3
korotkov_egor <korotkovegor10@gmail.com>
2024-06-19 18:40:34 +0300

    task 3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from cli import welcome_user
+from brain_games import cli
 
 
 def main():
     print('Welcome to the Brain Games!')
-    welcome_user()
+    cli.welcome_user()
 
 
 if __name__ == "__main__":
