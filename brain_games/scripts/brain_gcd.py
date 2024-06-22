97e0ccfc684ed58f304023b10f40418066f5fac6
korotkov_egor <korotkovegor10@gmail.com>
2024-06-22 11:37:15 +0300

    task 7

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import random
+import math
+
+from brain_games.scripts import base_for_games
+
+
+def find_solution():
+    first_number = random.randint(0, 100)
+    second_number = random.randint(0, 100)
+    expression = f'{first_number} {second_number}'
+    solution_of_expression = math.gcd(first_number, second_number)
+    return str(solution_of_expression), expression
+
+
+def greatest_common_divisor():
+    question = 'Find the greatest common divisor of given numbers.'
+    base_for_games.run_game(question, find_solution)
+
+
+
+if __name__ == '__main__':
+    greatest_common_divisor()
