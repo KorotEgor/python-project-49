64f17b8ca075de63866e80f44adb484c25d8f445
korotkov_egor <korotkovegor10@gmail.com>
2024-06-22 12:53:58 +0300

    task 9

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sympy
+
+import random
+
+from brain_games.scripts import base_for_games
+
+
+def find_solution():
+    expression = random.randint(1, 100000)
+    if sympy.isprime(expression):
+        return 'yes', expression
+    return 'no', expression
+
+
+def is_prime():
+    question = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    base_for_games.run_game(question, find_solution)
+
+
+if __name__ == '__main__':
+    is_prime()
